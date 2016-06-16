62c6d5f0e89322852bc2104d3822e95271f9dff5
Nic Gordon <gordonna@gmail.com>
2016-06-16 22:31:15 +1000

    Fix oxford comma usage for list of 2 items

--- app/lib/english.js
+++ app/lib/english.js
@@ -102,6 +102,6 @@ export default {
   toList: (arr) => {
     return arr.slice(0, -2).join(', ') + 
       (arr.slice(0, -2).length ? ', ' : '') + 
-      arr.slice(-2).join(', and ');
+      arr.slice(-2).join(arr.length > 2 ? ', and ' : ' and ');
   },
 }
